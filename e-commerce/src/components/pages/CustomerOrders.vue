85d9a221cc0f039013c039aeef9793f8b0d1d773
cclin <71698978+cclin-ariel@users.noreply.github.com>
2021-10-04 23:15:24 +0900

    modal of product detail

--- e-commerce/src/components/pages/CustomerOrders.vue
+++ e-commerce/src/components/pages/CustomerOrders.vue
@@ -16,21 +16,26 @@
             <h5 class="card-title">
               <a href="#" class="text-dark">{{ item.title }}</a>
             </h5>
-            <p class="card-text">{{item.description}}</p>
-            <div class="d-flex justify-content-between align-items-baseline"
-            >
-              <div class="h5" v-if="!item.price">{{ item.origin_price | currency }}</div>
+            <p class="card-text">{{ item.description }}</p>
+            <div class="d-flex justify-content-between align-items-baseline">
+              <div class="h5" v-if="!item.price">
+                {{ item.origin_price | currency }}
+              </div>
               <del class="h6">{{ item.origin_price | currency }}</del>
               <div class="h5">{{ item.price | currency }} for NOW!!</div>
             </div>
           </div>
           <div class="card-footer d-flex">
-            <button type="button" class="btn btn-outline-secondary btn-sm">
-              <i class="fas fa-spinner fa-spin"></i>
+            <button
+              type="button"
+              class="btn btn-outline-secondary btn-sm"
+              @click="getProduct(item.id)"
+            >
+              <i class="fas fa-spinner fa-spin" v-if="status.loagingItem === item.id"></i>
               查看更多
             </button>
             <button type="button" class="btn btn-outline-danger btn-sm ml-auto">
-              <i class="fas fa-spinner fa-spin"></i>
+              <i class="fas fa-spinner fa-spin" v-if="status.loagingItem === item.id"></i>
               加到購物車
             </button>
           </div>
@@ -39,16 +44,88 @@
 
       <!-- the end of product card -->
     </div>
+    <!-- the start of single product modal -->
+    <div
+      class="modal fade"
+      id="productModal"
+      tabindex="-1"
+      role="dialog"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">
+              {{ product.title }}
+            </h5>
+            <button
+              type="button"
+              class="close"
+              data-dismiss="modal"
+              aria-label="Close"
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <img :src="product.imageUrl" class="img-fluid" alt="" />
+            <blockquote class="blockquote mt-3">
+              <p class="mb-0">{{ product.content }}</p>
+              <footer class="blockquote-footer text-right">
+                {{ product.description }}
+              </footer>
+            </blockquote>
+            <div class="d-flex justify-content-between align-items-baseline">
+              <div class="h4" v-if="!product.price">
+                {{ product.origin_price }} 元
+              </div>
+              <del class="h6" v-if="product.price"
+                >原價 {{ product.origin_price }} 元</del
+              >
+              <div class="h4" v-if="product.price">
+                現在只要 {{ product.price }} 元
+              </div>
+            </div>
+            <select name="" class="form-control mt-3" v-model="product.num">
+              <option :value="num" v-for="num in 10" :key="num">
+                選購 {{ num }} {{ product.unit }}
+              </option>
+            </select>
+          </div>
+          <div class="modal-footer">
+            <div class="text-muted text-nowrap mr-3">
+              小計 <strong>{{ product.num * product.price }}</strong> 元
+            </div>
+            <button
+              type="button"
+              class="btn btn-primary"
+              @click="addtoCart(product.id, product.num)"
+            >
+              <i
+                class="fas fa-spinner fa-spin"
+                v-if="product.id === status.loadingItem"
+              ></i>
+              加到購物車
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- the end of single product modal -->
   </div>
 </template>
 <script>
-
+import $ from 'jquery'
 export default {
-
   data () {
     return {
       products: [],
-      isLoading: false
+      product: {},
+      isLoading: false,
+      status: {
+        loagingItem: ''
+      }
     }
   },
   methods: {
@@ -60,6 +137,17 @@ export default {
         console.log(response.data)
         vm.isLoading = false
         vm.products = response.data.products
+      })
+    },
+    getProduct (id) {
+      const api = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/product/${id}`
+      const vm = this
+      vm.status.loagingItem = id
+      this.$http.get(api).then(response => {
+        vm.product = response.data.product
+        $('#productModal').modal('show')
+        console.log(response.data)
+        vm.status.loagingItem = ''
       })
     }
   },
