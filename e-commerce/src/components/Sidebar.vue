8351910f0dd184d0e15c9e1346ceaf3f0cc45a4d
cclin <71698978+cclin-ariel@users.noreply.github.com>
2021-10-28 21:34:18 +0900

    costomerSite link in dashboard created

--- e-commerce/src/components/Sidebar.vue
+++ e-commerce/src/components/Sidebar.vue
@@ -53,6 +53,23 @@
           </router-link>
         </li>
       </ul>
+      <h6
+        class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted"
+      >
+        <span>Official Release</span>
+        <a class="link-secondary" href="#" aria-label="Add a new report">
+          <span data-feather="plus-circle"></span>
+        </a>
+      </h6>
+      <ul class="nav flex-column mb-2">
+        <li class="nav-item">
+          <router-link to="/customer_home" class="nav-link" href="#">
+            <span data-feather="file-text"></span>
+            <i class="fas fa-sitemap"></i>
+            Home
+          </router-link>
+        </li>
+      </ul>
     </div>
   </nav>
 </template>
